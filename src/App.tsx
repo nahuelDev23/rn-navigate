3f60dd9878934f9eadd415ed324c8dbdd43f74c9
nahueldev23 <nahuel.dev.23@gmail.com>
2024-06-05 11:37:28 -0300

    fin 113 - stackmenu + drawermenu custom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
-import {StackNavigator} from './presentation/routes/StackNavigator';
+import {SideMenuNavigator} from './presentation/routes/SideMenuNavigator';
 
 export const App = () => {
   return (
     <NavigationContainer>
-      <StackNavigator />
+      <SideMenuNavigator />
     </NavigationContainer>
   );
 };
---
+++ src/presentation/routes/SideMenuNavigator.tsx
@@ -0,0 +1,50 @@
+import {
+  DrawerContentComponentProps,
+  DrawerContentScrollView,
+  DrawerItemList,
+  createDrawerNavigator,
+} from '@react-navigation/drawer';
+import {StackNavigator} from './StackNavigator';
+import {ProfilScreen} from '../screens/profile/ProfilScreen';
+import {globalColors} from '../theme/theme';
+import {View, useWindowDimensions} from 'react-native';
+
+const Drawer = createDrawerNavigator();
+
+export const SideMenuNavigator = () => {
+  const dimensions = useWindowDimensions();
+  return (
+    <Drawer.Navigator
+      drawerContent={props => <CustomDrawerContent {...props} />}
+      screenOptions={{
+        headerShown: false,
+        drawerType: dimensions.width >= 768 ? 'permanent' : 'slide',
+        drawerActiveBackgroundColor: globalColors.primary,
+        drawerActiveTintColor: 'white',
+        drawerInactiveTintColor: globalColors.primary,
+        drawerItemStyle: {
+          borderRadius: 100,
+          paddingHorizontal: 20,
+        },
+      }}>
+      <Drawer.Screen name="StackNavigator" component={StackNavigator} />
+      <Drawer.Screen name="Profile" component={ProfilScreen} />
+    </Drawer.Navigator>
+  );
+};
+
+const CustomDrawerContent = (props: DrawerContentComponentProps) => {
+  return (
+    <DrawerContentScrollView>
+      <View
+        style={{
+          height: 200,
+          backgroundColor: globalColors.primary,
+          margin: 30,
+          borderRadius: 50,
+        }}
+      />
+      <DrawerItemList {...props} />
+    </DrawerContentScrollView>
+  );
+};
